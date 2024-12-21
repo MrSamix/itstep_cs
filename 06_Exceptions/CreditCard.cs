306c96012f97ef767ea5c2e20e520701d38c21b6
Oleksandr Panchuk <alekssen2008@gmail.com>
2024-12-21 22:42:46 +0200

    added hw06

---
+++ 06_Exceptions/CreditCard.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Exceptions
+{
+    internal class CreditCard
+    {
+        private string name = "NoName";
+        private string number = "NoNumber";
+        private DateTime expirationDate;
+        private int cvv;
+        private string owner = "NoOwner";
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be null or empty.");
+                }
+                name = value;
+            }
+        }
+        public string Number
+        {
+            get => number;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Number cannot be null or empty.");
+                }
+                else if (value.Length != 16)
+                {
+                    throw new ArgumentException("Number must be 16 digits long.");
+                }
+                number = value;
+            }
+        }
+        public DateTime ExpirationDate
+        {
+            get => expirationDate;
+            set
+            {
+                if (value < DateTime.Now)
+                {
+                    throw new ArgumentException("Expiration date mustn't be older than now");
+                }
+                expirationDate = value;
+            }
+        }
+        public int Cvv
+        {
+            get => cvv;
+            set
+            {
+                if (value < 100 || value > 999)
+                {
+                    throw new ArgumentException("CVV must be 3 digits long.");
+                }
+                cvv = value;
+            }
+        }
+        public string Owner
+        {
+            get => owner;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Owner cannot be null or empty.");
+                }
+                owner = value;
+            }
+        }
+        public CreditCard(string name, string number, DateTime expirationDate, int cvv, string owner)
+        {
+            Name = name;
+            Number = number;
+            ExpirationDate = expirationDate;
+            Cvv = cvv;
+            Owner = owner;
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}\nNumber: {Number}\nExpiration Date: {ExpirationDate.ToShortDateString()}\nCVV: {Cvv}\nOwner: {Owner}";
+        }
+    }
+}
---
+++ 06_Exceptions/Program.cs
@@ -0,0 +1,48 @@
+﻿using _06_Exceptions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+		try
+		{
+			CreditCard card = new CreditCard("Visa", "4444111188889999", new DateTime(2022,05,30), 555, "Oleksandr Panchuk"); // exception
+			Console.WriteLine(card);
+		}
+		catch (Exception ex)
+		{
+			Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("ERROR:");
+			Console.WriteLine("Message: " + ex.Message);
+            Console.WriteLine("Source: " + ex.Source);
+            Console.ResetColor();
+        }
+        Console.WriteLine();
+        try
+        {
+            CreditCard card = new CreditCard("Visa", "4444111188889999", new DateTime(2025, 05, 30), 6666, "Oleksandr Panchuk"); // exception
+        }
+        catch (Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("ERROR:");
+            Console.WriteLine("Message: " + ex.Message);
+            Console.WriteLine("Source: " + ex.Source);
+            Console.ResetColor();
+        }
+        Console.WriteLine();
+        try
+        {
+            CreditCard card = new CreditCard("Visa", "4444111188889999", new DateTime(2025, 05, 30), 555, "Oleksandr Panchuk"); // normal
+            Console.WriteLine(card);
+        }
+        catch (Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("ERROR:");
+            Console.WriteLine("Message: " + ex.Message);
+            Console.WriteLine("Source: " + ex.Source);
+            Console.ResetColor();
+        }
+    }
+}
