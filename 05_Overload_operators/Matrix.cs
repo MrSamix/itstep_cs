b996308f63dfcc955ccbe2b8fa1591b20e337ce5
Oleksandr Panchuk <alekssen2008@gmail.com>
2024-12-22 22:01:14 +0200

    added hw05

---
+++ 05_Overload_operators/Matrix.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace _05_Overload_operators
+{
+    internal class Vector
+    {
+        private double x;
+        private double y;
+
+        public double X { get => x; set => x = value; }
+        public double Y { get => y; set => y = value; }
+        public Vector(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double Length()
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            return new Vector(v1.x + v2.x, v1.y + v2.y);
+        }
+
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return new Vector(v1.x - v2.x, v1.y - v2.y);
+        }
+
+        public static Vector operator *(Vector v, double num)
+        {
+            return new Vector(v.x * num, v.y * num);
+        }
+
+        public static double operator *(Vector v1, Vector v2)
+        {
+            return v1.x * v2.x + v1.y * v2.y;
+        }
+
+        public static bool operator ==(Vector v1, Vector v2)
+        {
+            if (ReferenceEquals(v1, v2))
+                return true;
+            if (ReferenceEquals(v1, null) || ReferenceEquals(v2, null))
+                return false;
+            return v1.x == v2.x && v1.y == v2.y;
+        }
+
+        public static bool operator !=(Vector v1, Vector v2)
+        {
+            return !(v1 == v2);
+        }
+
+        public static explicit operator double(Vector v)
+        {
+            return v.Length();
+        }
+
+        public static implicit operator Vector(double num)
+        {
+            return new Vector(num, 0);
+        }
+
+        public static Vector operator ++(Vector v)
+        {
+            return new Vector(v.x + 1, v.y + 1);
+        }
+
+        public static Vector operator --(Vector v)
+        {
+            return new Vector(v.x - 1, v.y - 1);
+        }
+
+        public static Vector operator -(Vector v)
+        {
+            return new Vector(-v.x, -v.y);
+        }
+
+        public static bool operator true(Vector v)
+        {
+            return v.x != 0 || v.y != 0;
+        }
+
+        public static bool operator false(Vector v)
+        {
+            return v.x == 0 && v.y == 0;
+        }
+
+        public double this[int index]
+        {
+            get
+            {
+                if (index == 0)
+                    return x;
+                else if (index == 1)
+                    return y;
+                else
+                    return -1;
+            }
+            set
+            {
+                if (index == 0)
+                    x = value;
+                else if (index == 1)
+                    y = value;
+                else
+                    y = -1;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"({x}, {y})";
+        }
+    }
+}
---
+++ 05_Overload_operators/Program.cs
@@ -0,0 +1,40 @@
+﻿using _05_Overload_operators;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Vector v1 = new Vector(1, 2);
+        Vector v2 = new Vector(3, 4);
+        Vector v3 = v1 + v2;
+        Vector v4 = v1 - v2;
+        Vector v5 = v1 * 2;
+        double v6 = v1 * v2;
+        double v7 = (double)v1;
+        Vector v8 = v7;
+        Console.WriteLine($"v1 = {v1}");
+        Console.WriteLine($"v2 = {v2}");
+        Console.WriteLine($"v1 + v2 = {v3}");
+        Console.WriteLine($"v1 - v2 = {v4}");
+        Console.WriteLine($"v1 * 2 = {v5}");
+        Console.WriteLine($"v1 * v2 = {v6}");
+        Console.WriteLine($"v1 to double(explicit) = {v7}");
+        Console.WriteLine($"v7(double) to Vector(implicit) = {v8}");
+        Console.WriteLine();
+        Console.WriteLine($"v1 == v2? {v1 == v2}");
+        Console.WriteLine($"v1 != v2? {v1 != v2}");
+        Console.WriteLine($"v1 != v3? {v1 != v3}");
+        Console.WriteLine($"v1 == v3? {v1 == v3}");
+        Console.WriteLine($"v1 != v4? {v1 != v4}");
+        Console.WriteLine($"v1 == v4? {v1 == v4}");
+        Console.WriteLine($"v1 != v5? {v1 != v5}");
+        Console.WriteLine($"v1 == v5? {v1 == v5}");
+
+        Console.WriteLine();
+
+        Console.WriteLine($"v3 = {v3}");
+        Console.WriteLine($"v3 += v1 = {v3 += v1}");
+        Console.WriteLine($"v3 -= v1 = {v3 -= v1}");
+        Console.WriteLine($"v3 *= 2 = {v3 *= 2}");
+    }
+}
