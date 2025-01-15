ac3ed53676d56b2c41bc51de4931cce61c046ceb
Oleksandr Panchuk <alekssen2008@gmail.com>
2025-01-15 16:24:09 +0200

    added hw09

---
+++ 09_Structure/Program.cs
@@ -0,0 +1,23 @@
+﻿using _09_Structure;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Vector3D v1 = new Vector3D(1, 2, 3);
+        Vector3D v2 = new Vector3D(4, 5, 6);
+
+        Console.WriteLine("v1: " + v1);
+        Console.WriteLine("v2: " + v2);
+        
+
+        Vector3D v3 = v1 + v2;
+        Console.WriteLine("v1 + v2: " + v3);
+        
+        Vector3D v4 = v2 - v1;
+        Console.WriteLine("v2 - v1: " + v4);
+        
+        Vector3D mult = v2.Mult(2);
+        Console.WriteLine("v2 * 2: " + mult);
+    }
+}
---
+++ 09_Structure/Vector3D.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Structure
+{
+    public struct Vector3D
+    {
+        public double X { get; }
+        public double Y { get; }
+        public double Z { get; }
+
+        public Vector3D(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public Vector3D Mult(double scalar)
+        {
+            return new Vector3D(X * scalar, Y * scalar, Z * scalar);
+        }
+
+        public static Vector3D operator +(Vector3D v1, Vector3D v2)
+        {
+            return new Vector3D(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+        }
+
+        public static Vector3D operator -(Vector3D v1, Vector3D v2)
+        {
+            return new Vector3D(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
+        }
+
+        public override string ToString()
+        {
+            return $"({X}, {Y}, {Z})";
+        }
+    }
+}
