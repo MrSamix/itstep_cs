b807430ae9e913089b25f190daefa5169c83587e
Oleksandr Panchuk <alekssen2008@gmail.com>
2025-01-15 16:14:54 +0200

    added hw08

---
+++ 08_Interfaces/Array.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Interfaces
+{
+    internal class Array : ICalc, IOutput2, ICalc2
+    {
+        private int[] ints;
+        public Array(params int[] numbers)
+        {
+            ints = numbers;
+        }
+
+        
+
+        public void Show()
+        {
+            
+            //foreach (var i in ints)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            Console.WriteLine(String.Join(", ", ints)); // для зручності, щоб виводило в рядок
+        }
+
+        public void Show(string info)
+        {
+            if (!String.IsNullOrWhiteSpace(info))
+            {
+                Console.WriteLine(info);
+            }
+            Show();
+        }
+
+        public int Less(int valueToCompare)
+        {
+            return ints.Count(i => i < valueToCompare);
+        }
+        public int Greater(int valueToCompare)
+        {
+            return ints.Count(i => i > valueToCompare);
+        }
+
+        public void ShowEven()
+        {
+            int[] even_ints = new int[0];
+            foreach (var i in ints)
+            {
+                if (i % 2 == 0)
+                {
+                    System.Array.Resize(ref even_ints, even_ints.Length + 1);
+                    even_ints[even_ints.Length - 1] = i;
+                }
+            }
+            Console.WriteLine(String.Join(", ", even_ints));
+        }
+
+        public void ShowOdd()
+        {
+            int[] odd_ints = new int[0];
+            foreach (var i in ints)
+            {
+                if (i % 2 != 0)
+                {
+                    System.Array.Resize(ref odd_ints, odd_ints.Length + 1);
+                    odd_ints[odd_ints.Length - 1] = i;
+                }
+            }
+            Console.WriteLine(String.Join(", ", odd_ints));
+        }
+
+        public int CountDistinct()
+        {
+            return ints.Distinct().Count();
+        }
+
+        public int EqualToValue(int valueToCompare)
+        {
+            return ints.Count(i => i == valueToCompare);
+        }
+    }
+}
---
+++ 08_Interfaces/Program.cs
@@ -0,0 +1,20 @@
+﻿using _08_Interfaces;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        _08_Interfaces.Array arr = new _08_Interfaces.Array(2, 5, 4, 7, 8, 9, 6, 3, 3);
+
+        arr.Show();
+
+        Console.WriteLine($"Counts of number less 4 = {arr.Less(4)}");
+        Console.WriteLine($"Counts of number greater 5 = {arr.Greater(5)}");
+
+        Console.WriteLine("Even numbers: ");
+        arr.ShowEven();
+        Console.WriteLine("Odd numbers: ");
+        arr.ShowOdd();
+        Console.WriteLine($"Count unique numbers in arr = {arr.CountDistinct()}");
+        Console.WriteLine($"Count of numbers equal to 3 = {arr.EqualToValue(3)}");
+    }
+}
