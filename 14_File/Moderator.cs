4934759f20453e0e4ec299096bfd57231e6b63f9
Oleksandr Panchuk <alekssen2008@gmail.com>
2025-01-26 22:10:39 +0200

    added hw14

---
+++ 14_File/Moderator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_File
+{
+    internal class Moderator
+    {
+        public string Text_path { get; set; }
+        public string BannedWords_path { get; set; }
+        public Moderator(string text_pathfile, string bannedwords_pathfile)
+        {
+            Text_path = text_pathfile;
+            BannedWords_path = bannedwords_pathfile;
+        }
+        public void CheckText()
+        {
+            string text, bannedwords;
+            try
+            {
+                text = File.ReadAllText(Text_path);
+            }
+            catch (Exception) 
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("File \"" + Text_path + "\" not opened! Maybe, you don't create it or enter incorrect path. Please create this file!");
+                Console.ResetColor();
+                return;
+            }
+            try
+            {
+                bannedwords = File.ReadAllText(BannedWords_path);
+            }
+            catch (Exception) 
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("File \"" + BannedWords_path + "\" not opened! Maybe, you don't create it or enter incorrect path. Please create this file!");
+                Console.ResetColor();
+                return;
+            }
+            Console.WriteLine("text: \n" + text);
+            Console.WriteLine("Banwords: " + bannedwords);
+
+            string[] banwords = bannedwords.Split(' ');
+
+            foreach (string word in banwords) 
+            {
+                if (text.Contains(word))
+                {
+                    text = text.Replace(word, new string('*', word.Length));
+                }
+            }
+            Console.WriteLine("Result text: \n" + text);
+        }
+    }
+}
---
+++ 14_File/Program.cs
@@ -0,0 +1,10 @@
+﻿using _14_File;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Moderator moderator = new Moderator("../../../text.txt", "../../../banwords.txt");
+        moderator.CheckText();
+    }
+}
